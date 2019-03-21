e7a23260a85147604980e7b738142b703bd5fe7d
Dayton <daytonlowell@me.com>
2019-03-20 21:54:28 -0500

    Use Object.freeze on transliterateMap and weight contsants to ensure they don't get mutated

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const validationCodes = require('./validation-codes')
 const CHECKSUM_INDEX = 8
 const SUCCESS_CODE = 'SUCCESS'
+const weight = Object.freeze([ 8, 7, 6, 5, 4, 3, 2, 10, 0, 9, 8, 7, 6, 5, 4, 3, 2 ])
+const transliterateMap = Object.freeze({
+	//I, O, & Q are invalid
+	A: 1, B: 2, C: 3, D: 4, E: 5, F: 6, G: 7, H: 8, J: 1, K: 2, L: 3, M: 4, N: 5, P: 7, R: 9, S: 2, T: 3, U: 4, V: 5, W: 6, X: 7, Y: 8, Z: 9, 0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9,
+})
 
 function makeValueMap(vin) {
-	const transliterateMap = {
-		//I, O, & Q are invalid
-		A: 1, B: 2, C: 3, D: 4, E: 5, F: 6, G: 7, H: 8, J: 1, K: 2, L: 3, M: 4, N: 5, P: 7, R: 9, S: 2, T: 3, U: 4, V: 5, W: 6, X: 7, Y: 8, Z: 9, 0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9,
-	}
-
 	return vin.map((character, index) => {
 		if (index === CHECKSUM_INDEX) {
 			return 0
@@ -22,8 +22,6 @@ function makeValueMap(vin) {
 }
 
 function checkChecksum(value, checksumCharacter) {
-	const weight = [ 8, 7, 6, 5, 4, 3, 2, 10, 0, 9, 8, 7, 6, 5, 4, 3, 2 ]
-
 	const sum = value
 		.map((val, index) => val * weight[index])
 		.reduce((sum, val) => sum + val, 0)
